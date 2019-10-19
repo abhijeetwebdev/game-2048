29a3244fd1b1d9067a34150bdc3213d278b0f0ee
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-10-19 16:33:11 +0530

    add: foundation + create dynamic board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { BlockComponent } from './block/block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    BlockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
