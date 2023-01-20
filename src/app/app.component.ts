da93bc2475907a856b0ac737b2915836e80b4635
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-20 11:09:56 +0530

    add: fill random blocks to get started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
   boardCreated = false;
   boardSize = 4;
   blocks: any[];
+  randomNums = [2, 4];
 
   constructor() {
     this.blocks = [];
@@ -24,7 +25,6 @@ export class AppComponent implements OnInit {
   }
 
   blocksCreate() {
-    // alert(this.boardSize);
     for (let i = 0; i < this.boardSize; i++) {
       this.blocks[i] = [];
       for (let j = 0; j < this.boardSize; j++) {
@@ -35,7 +35,27 @@ export class AppComponent implements OnInit {
   }
 
   boardDisplay() {
+    this.initialBlocks();
     this.boardCreated = true;
+  }
+
+  initialBlocks() {
+    const block1 = this.blockSelectRandom();
+    this.blockFillRandomNum(block1[0], block1[1]);
+
+    const block2 = this.blockSelectRandom();
+    this.blockFillRandomNum(block2[0], block2[1]);
+  }
+
+  /**
+   * returns array [row, col] of block location
+   */
+  blockSelectRandom() {
+    return [Math.floor(Math.random() * ((this.boardSize - 1) - 0 + 1)) + 0, Math.floor(Math.random() * ((this.boardSize - 1) - 0 + 1)) + 0];
+  }
+
+  blockFillRandomNum(row, col) {
+    this.blocks[row][col] = this.randomNums[Math.floor(Math.random() * this.randomNums.length)];
   }
 
 }
