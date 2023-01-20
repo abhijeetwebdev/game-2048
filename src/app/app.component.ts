a470a9cc6604b8a6ef032c0c946282429bcad78d
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-20 11:09:56 +0530

    add: foundation + create dynamic board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ather-assignment';
+export class AppComponent implements OnInit {
+
+  boardCreated = false;
+  boardSize = 4;
+  blocks: any[];
+
+  constructor() {
+    this.blocks = [];
+  }
+
+  ngOnInit() {
+    this.boardCreate();
+  }
+
+  boardCreate() {
+    this.blocksCreate();
+  }
+
+  blocksCreate() {
+    // alert(this.boardSize);
+    for (let i = 0; i < this.boardSize; i++) {
+      this.blocks[i] = [];
+      for (let j = 0; j < this.boardSize; j++) {
+        this.blocks[i].push(0);
+      }
+    }
+    this.boardDisplay();
+  }
+
+  boardDisplay() {
+    this.boardCreated = true;
+  }
+
 }
