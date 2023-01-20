ae6659d9689e5abcb7f775fc14a619d5fdcae7e8
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-20 11:27:49 +0530

    rename: from my interview assignment to the game

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ather-assignment!');
+    expect(page.getTitleText()).toEqual('Welcome to game-2048!');
   });
 
   afterEach(async () => {
